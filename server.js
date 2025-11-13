3341d71de703396f8c4236f0d5fa08bb5ef84af8
Wayne <waynetavis123@gmail.com>
2025-11-14 01:21:47 +0300

    Update Plant Nursery Manager project description and fix server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 app.use(express.static(__dirname));
 
